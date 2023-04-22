14705979f3aac605d5805667878b5595200e52a9
JoaoSerafim1 <129481962+JoaoSerafim1@users.noreply.github.com>
2023-04-22 01:53:18 -0300

    Upload inicial
    Upload inicial dos arquivos do banco de dados

---
+++ BackupDoDBTicketitas/ticketitassql80_ingressos.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ticketitassql80
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ingressos`
+--
+
+DROP TABLE IF EXISTS `ingressos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ingressos` (
+  `idIngresso` int NOT NULL,
+  `setor` int NOT NULL DEFAULT '0',
+  `perfilDePreco` int NOT NULL DEFAULT '0',
+  `valor` float NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idIngresso`),
+  CONSTRAINT `CHK_perfilDePreco` CHECK (((`perfilDePreco` >= 0) and (`perfilDePreco` <= 2))),
+  CONSTRAINT `CHK_setor` CHECK (((`setor` >= 0) and (`setor` <= 2))),
+  CONSTRAINT `ingressos_chk_1` CHECK ((`setor` >= 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingressos`
+--
+
+LOCK TABLES `ingressos` WRITE;
+/*!40000 ALTER TABLE `ingressos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ingressos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-22  1:44:46
